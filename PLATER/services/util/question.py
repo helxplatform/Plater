6e56bbfac6aa12286ea2256a809a9ba9b8777077
YaphetKG <kebedey@renci.org>
2020-11-16 20:16:28 -0500

    Redis graph integration

--- PLATER/services/util/question.py
+++ PLATER/services/util/question.py
@@ -1,8 +1,7 @@
 import copy
-from functools import reduce
 from PLATER.services.util.graph_adapter import GraphInterface
 import time
-from reasoner.cypher import get_query
+
 
 class Question:
 
@@ -33,39 +32,11 @@ class Question:
         :param graph_interface: interface for neo4j
         :return: None
         """
-        cypher = self.compile_cypher()
-        print(cypher)
         s = time.time()
-        results = await graph_interface.run_cypher(cypher)
+        await graph_interface.answer_trapi_question(self._question_json['query_graph'])
         end = time.time()
         print(f'grabbing results took {end - s}')
-        results_dict = graph_interface.convert_to_dict(results)
-        self._question_json.update(results_dict[0])
         return self._question_json
-
-    def __validate(self):
-        assert Question.QUERY_GRAPH_KEY in self._question_json, "No question graph in json."
-        question_graph = self._question_json[Question.QUERY_GRAPH_KEY]
-        assert Question.NODES_LIST_KEY in question_graph, "No nodes in query graph"
-        assert isinstance(question_graph[Question.NODES_LIST_KEY], list), "Expected nodes to be list"
-        assert Question.EDGES_LIST_KEY in question_graph, "No edges in query graph"
-        assert isinstance(question_graph[Question.EDGES_LIST_KEY], list), "Expected edges to be list"
-        for node in question_graph[Question.NODES_LIST_KEY]:
-            assert Question.TYPE_KEY in node , f"Expected {Question.TYPE_KEY} in {node}"
-            assert 'id' in node, f"Expected `id` in {node}"
-        for edge in question_graph[Question.EDGES_LIST_KEY]:
-            assert 'id' in edge, f"Expected `id` in {edge}"
-            assert Question.SOURCE_KEY in edge, f"Expected {Question.SOURCE_KEY} in {edge}"
-            assert Question.TARGET_KEY in edge, f"Expected {Question.TARGET_KEY} in {edge}"
-        # make sure everything mentioned in edges is actually refering something in the node list.
-        node_ids = list(map(lambda node: node['id'], question_graph[Question.NODES_LIST_KEY]))
-        mentions = reduce(lambda accu, value: accu + value,
-                          list(map(lambda edge: [
-                              edge[Question.SOURCE_KEY],
-                              edge[Question.TARGET_KEY]
-                          ], question_graph[Question.EDGES_LIST_KEY])), [])
-        assert reduce(lambda x, y: x and (y in node_ids), mentions, True), "Some edge mentions don't have matching " \
-                                                                           "nodes. Please check question graph."
 
     @staticmethod
     def transform_schema_to_question_template(graph_schema):
@@ -138,35 +109,60 @@ class Question:
 
 
 if __name__ == '__main__':
-    schema  = {
-      "gene": {
-        "biological_process_or_activity": [
-          "actively_involved_in"
-        ],
-        "named_thing": [
-          "similar_to"
-        ]
-      },
-      "named_thing": {
-        "chemical_substance": [
-          "similar_to"
-        ],
-        "named_thing": [
-          "similar_to"
-        ]
-      }
+    q_graph = {
+    "query_graph": {
+      "nodes": [
+        {
+          "id": "n0",
+          "type": "chemical_substance",
+"curie":"CHEBI:39385"
+        }, { "id" :"n1", "type":"named_thing", "set": true}
+      ],
+      "edges": [
+          {"id": "e0", "source_id": "n0", "target_id": "n1"}
+                ]
     }
-    import json
-    questions = Question.transform_schema_to_question_template(schema)
-    print(questions)
-    question = Question(questions[0])
-    # questions[0]['query_graph']['nodes'][1]['curie'] = ''
-    questions[0]['query_graph']['nodes'][1]['type'] = 'disease'
-    del questions[0]['query_graph']['edges'][0]['type']
-    questions[0]['query_graph']['nodes'][0]['type'] = 'information_content_entity'
-    q2 = Question(questions[0])
-    ans = q2.answer(graph_interface=GraphInterface('localhost','7474', ('neo4j', 'neo4jkp')))
+  }
+    driver = GraphInterface('h', 'b', 'c')
+    question = Question(q_graph)
     import asyncio
-    event_loop = asyncio.get_event_loop()
-    result = event_loop.run_until_complete(ans)
-    print(json.dumps(result, indent=2))
+    asyncio.run(question.answer(graph_interface=driver))
+
+
+
+
+
+
+
+    # schema  = {
+    #   "gene": {
+    #     "biological_process_or_activity": [
+    #       "actively_involved_in"
+    #     ],
+    #     "named_thing": [
+    #       "similar_to"
+    #     ]
+    #   },
+    #   "named_thing": {
+    #     "chemical_substance": [
+    #       "similar_to"
+    #     ],
+    #     "named_thing": [
+    #       "similar_to"
+    #     ]
+    #   }
+    # }
+    # import json
+    # questions = Question.transform_schema_to_question_template(schema)
+    # print(questions)
+    # question = Question(questions[0])
+    # # questions[0]['query_graph']['nodes'][1]['curie'] = ''
+    # questions[0]['query_graph']['nodes'][1]['type'] = 'disease'
+    # del questions[0]['query_graph']['edges'][0]['type']
+    # questions[0]['query_graph']['nodes'][0]['type'] = 'information_content_entity'
+    # q2 = Question(questions[0])
+    # ans = q2.answer(graph_interface=GraphInterface('localhost','7474', ('neo4j', 'neo4jkp')))
+    # import asyncio
+    # event_loop = asyncio.get_event_loop()
+    # result = event_loop.run_until_complete(ans)
+    # print(json.dumps(result, indent=2))
